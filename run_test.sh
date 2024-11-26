a25e2a7899877ff6d1dccb5ac590066abe39e978
Ivan Sanchez <ivan@gourame.com>
2024-11-26 16:45:33 +0000

    Initial commit

---
+++ run_test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -o errexit
+set -o pipefail
+set -o nounset
+
+BASE_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+docker run -p 8000:80 -d --name httpbin kennethreitz/httpbin
+
+sleep 2
+
+curl -v http://localhost:8000/json
+
+docker stop httpbin
+docker rm httpbin
